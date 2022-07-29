56367ab17aadd558c1bb20f5b5eca6b19a064270
Jacob A Stout <92182042+JediJake66@users.noreply.github.com>
2022-07-29 10:04:13 -0400

    Added junit tests

--- RESTfulAPIDemo1/src/test/java/com/genspark/RESTfulAPIDemo1/ResTfulApiDemo1ApplicationTests.java
+++ RESTfulAPIDemo1/src/test/java/com/genspark/RESTfulAPIDemo1/ResTfulApiDemo1ApplicationTests.java
@@ -1,13 +1,35 @@
 package com.genspark.RESTfulAPIDemo1;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import com.genspark.RESTfulAPIDemo1.Controller.Controller;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ResTfulApiDemo1ApplicationTests {
 
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private Controller controller;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
+
+	@Test
+	public void greetingShouldReturnDefaultMessage() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+				String.class).contains("<HTML><H1>Welcome to Jake's Demo Company!</H1></HTML>"));
 	}
 
 }
